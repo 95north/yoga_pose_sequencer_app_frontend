a775734ee6e7e15b60127991eda7e3b9283428f9
github username <github email address>
2019-07-29 14:48:36 -0400

    made cards and added some css

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-const poseCardContainer = getElementById("poses-div")
-
-fetch("http://localhost:3000/poses").then(res => (res.json() ))
-.then(function (res){
-    console.log(res);
-})
+const poseCardContainer = document.querySelector("#poses-div")
 
 
-function createPoseCard(aPoseApi){
-    c = document.createElement("DIV");
-    c.style.backgroundColor = "sky blue";
-    n = document.createElement("P");
-    n.innerText = aPoseApi.name + " - " + aPoseApi.sanskrit_name;
-
-    c.appendChild(n);
-    poseCardContainer.appendChild(c);
-    
-    
 
 
-}
+function createPoseCard(){
+    // c = document.createElement("DIV");
+    // c.style.backgroundColor = "sky blue";
+    // n = document.createElement("P");
+    // n.innerText = aPoseApi.name + " - " + aPoseApi.sanskrit_name;
+    //
+    // c.appendChild(n);
+    // poseCardContainer.appendChild(c);
+
+    fetch("http://localhost:3000/poses")
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(pose){
+      pose.forEach(function(pose){
+        const poseCards = document.createElement("div")
+        poseCards.classList.add("card")
+        let poseHeaderName = document.createElement("h2")
+        poseHeaderName.innerText = pose.pose_name
+        poseCards.appendChild(poseHeaderName)
+        let poseImage = document.createElement("img")
+        poseImage.src = pose.photo_url
+        poseImage.classList.add("center")
+        poseCards.appendChild(poseImage)
+        let poseIntensity = document.createElement("h3")
+        poseIntensity.innerText = "Intensity: "+ pose.intensity
+        poseCards.appendChild(poseIntensity)
+        let posePurpose = document.createElement("h3")
+        posePurpose.innerText = "Purpose: "+pose.purpose
+        poseCards.appendChild(posePurpose)
+        let poseProp = document.createElement("h3")
+        poseProp.innerText = "Props: "+ pose.prop
+        poseCards.appendChild(poseProp)
+        let addPoseButton = document.createElement("button")
+        addPoseButton.classList.add("button")
+        addPoseButton.innerText = "Add Pose"
+        poseCards.appendChild(addPoseButton)
+        poseCardContainer.appendChild(poseCards)
+
+      })
+
+
+    })
+} //edn of createPoseCard
+
+
+
+
+createPoseCard()
